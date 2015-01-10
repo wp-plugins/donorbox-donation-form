983593bc250038149de0c3063b8f379f0205ad0c
rebelidealist <rebelidealist@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-10 11:03:02 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/donorbox-donation-form/trunk@1064551 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ donorbox_embed_campaign.php
@@ -0,0 +1,163 @@
+<?php
+/*
+Plugin Name: Donorbox Donation Form
+Plugin URI: https://donorbox.org
+Description: This plugin will embed Donorbox Donation Form to your site using shortcode '[donate]' and '[donate-with-info]'. The settings is very simple. One input box for either the full Donorbox url e.g. https://donorbox.org/campaign-id or the part after the '/' 'campaign_id'. No embed code needed. We will generate the embed code for you!
+Author: rebelidealist
+Author URI: https://donorbox.org
+Tags: donorbox, donation, embed, campaign, form, shortcode
+Version: 1.0
+License: GPLv2 or later.
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/** 
+ * Donorbox class
+ */
+class Donorbox_donation_form {
+    public $options; // variable to hold the options values
+    
+    /** 
+     * Class constructor
+     */
+    public function __construct() {
+        $this->options = get_option('donorbox_embed_campaign_options'); // get the existing plugin options
+        $this->donorbox_register_settings_and_fields(); // invoke to register the plugin settings and admin sections
+    }
+
+    /** 
+     * Generate the admin settings page
+     */
+    public function donorbox_display_options_page() { ?>
+        <div class="wrap">
+            <?php screen_icon(); ?>
+            <h2>Donorbox Settings</h2>
+            <form method="post" action="options.php">
+                <?php 
+                settings_fields('donorbox_embed_campaign_options');
+                do_settings_sections(__FILE__);
+                ?>
+                <p class="submit">
+                    <input name="submit" type="submit" class="button-primary" value="Save Changes" />
+                </p>
+            </form>
+        </div><?php
+    }
+
+    /** 
+     * Add menu page
+     */
+    public function donorbox_add_menu_page() {
+        add_options_page('Donorbox', 'Donorbox', 'administrator', __FILE__, array('Donorbox_donation_form','donorbox_display_options_page'));
+    }
+
+    /** 
+     * Register fields and sections
+     */
+    public function donorbox_register_settings_and_fields() {
+        register_setting('donorbox_embed_campaign_options', 'donorbox_embed_campaign_options');
+        add_settings_section('donorbox_campaign_settings_section', 'Campaign Settings', array($this, 'donorbox_embed_campaign_callback'), __FILE__);
+        add_settings_field('donorbox_embed_campaign_id', 'Campaign ID', array($this, 'donorbox_embed_campaign_id_settings'), __FILE__, 'donorbox_campaign_settings_section');
+    }
+
+    /** 
+     * Callback - can be used for extending features
+     */
+    public function donorbox_embed_campaign_callback() {}
+
+    /** 
+     * Campaign details section
+     */
+    public function donorbox_embed_campaign_id_settings() { ?>
+        <input name="donorbox_embed_campaign_options[donorbox_embed_campaign_id]" type="text" value="<?php echo $this->options['donorbox_embed_campaign_id']; ?>" class="regular-text" />
+        <br/><br/>
+        <p class="description"><strong><i>Instructions</i></strong></p>
+        <p class="description">1. After signing up on <a href="https://donorbox.org" target="_blank">Donorbox.org</a>, create a donation campaign from the dashboard.</p>
+        <p class="description">2. Paste in either the full Donorbox campaign url e.g. https://donorbox.org/campaign-id or the part after the '/' 'campaign-id'.</p>
+        <p class="description">3. Use the shortcode <strong>[donate]</strong> to embed the donation form. You can also use the shortcode <strong>[donate-with-info]</strong> which will include the campaign description and legal disclaimer that was on Donorbox.</p>
+        <?php
+    }
+}
+
+/** 
+ * Add to admin menu
+ */
+function donorbox_add_options_page_function() {
+    Donorbox_donation_form::donorbox_add_menu_page();
+}
+
+/** 
+ * Class object creation
+ */
+function donorbox_initiate_class() {
+    new Donorbox_donation_form();
+}
+
+/** 
+ * Get the meta options file and set settings accordingly
+ */
+function donorbox_embed_campaign_set_plugin_meta($links, $file) {
+    $plugin = plugin_basename(__FILE__);
+    if ($file == $plugin) {
+        return array_merge(
+            $links,
+            array( sprintf( '<a href="options-general.php?page=%s">%s</a>', $plugin, __('Settings') ) )
+        );
+    }
+    return $links;
+}
+
+/** 
+ * Generalized function to generate iframe code - based on input parameter adds or skips info section
+ * $info_details - Parameter - Determines whether to add info section or not
+ * Input value to add info - 'with-info'
+ * Returns iframe embed code
+ */
+function generate_donorbox_iframe_src($info_details) {
+    // this points to the base donorbox domain - embed codes are generated based on this
+    $donorbox_domain = 'https://donorbox.org';
+
+    // get the existing options entries for the plugin
+    $options = get_option('donorbox_embed_campaign_options');
+
+    $donorbox_campaign_input = $options['donorbox_embed_campaign_id']; // get the campaign id
+    $campaign_keys = parse_url($donorbox_campaign_input); // parse the url
+    $path = explode("/", $campaign_keys['path']); // splitting the path
+    $campaign_id = end($path); // get the value of the last element
+    // if user entered a slash at end of the url, then check if a valid campaign id can be extracted from the url or not
+    if (empty($campaign_id)) {
+        $campaign_id = prev($path); // traceback to previous url segments
+    }
+
+    // if parameter is there to add info, then append the attribute to iframe src
+    if ($info_details === 'with-info') {
+        $campaign_id = $campaign_id.'?show_content=true';
+    }
+
+    // generate the iframe code
+    $donorbox_iframe_embed_code = '<iframe src="'.$donorbox_domain.'/embed/'.$campaign_id.'" width="100%" height="800px" seamless="seamless" name="donorbox" frameborder="0" scrolling="no"></iframe>';
+    // return the embed code to calling event i.e. shortcode replacement
+    return $donorbox_iframe_embed_code;
+}
+
+/** 
+ * Replace the shortcode with the embed code - no info
+ */
+function display_donorbox_iframe($atts) {
+    return generate_donorbox_iframe_src('');
+}
+
+/** 
+ * Replace the shortcode with the embed code - with info
+ */
+function display_donorbox_iframe_with_info($atts) {
+    return generate_donorbox_iframe_src('with-info');
+}
+
+add_shortcode( 'donate', 'display_donorbox_iframe' ); // add [donate] shortcode
+add_shortcode( 'donate-with-info', 'display_donorbox_iframe_with_info' ); // add [donate-with-info] shortcode
+add_action('admin_menu', 'donorbox_add_options_page_function'); // add item to admin menu
+add_action('admin_init', 'donorbox_initiate_class'); // plugin initialization action
+add_filter( 'plugin_row_meta', 'donorbox_embed_campaign_set_plugin_meta', 10, 2 ); // plugin meta options
+
+?>
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== Donorbox Donation Form ===
+Contributors: rebelidealist
+Plugin URI: https://donorbox.org
+Author URI: https://donorbox.org
+Donate link: https://donorbox.org
+Tags: donorbox, donation, embed, campaign, shortcode
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin to easily embed Donorbox Donation Form to your site
+
+== Description ==
+
+Donorbox is an easy to use, optimized donation payment processing form. It has recurring donations, suggested amounts, donor information collection, supports several major currencies. All your donor data can be easily exported.
+
+Donorbox is FREE for your first $2,000 donation EVERY month. The credit card processing fee from <a href="http://stripe.com">Stripe</a> is 2.9% + 0.30. It takes no more than 10 mintues to start fundraising. <a href="https://donorbox.org">Signup at Donorbox.org</a>
+
+This plugin will embed Donorbox Donation Form to your site using shortcode '[donate]' and '[donate-with-info]'. The settings is very simple. One input box for either the full Donorbox url e.g. https://donorbox.org/campaign-id or the part after the '/' 'campaign_id'. No embed code needed. We will generate the embed code for you!
+
+Dashboard > Settings > Donorbox
+
+== Installation ==
+
+1. Unzip the zipped file and upload to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Manage individual options from Dashboard > Settings > Donorbox
+4. After signing up on Donorbox.org, create a donation campaign from the dashboard.
+5. Paste in the Campaign url: [input box]
+6. Use the shortcode [donate] to embed the donation form. You can also use the shortcode [donate-with-info] which will include the campaign description and legal disclaimer that was on Donorbox.
+
+
+== Frequently Asked Questions ==
+
+= What is the shortcode I need to use? =
+
+Use the following shortcode in your posts / pages or the template file: [donate]  or [donate-with-info]
+The plugin will replace the shortcode with embed frame using the settings provided in admin.
+
+= Where is the settings page? =
+
+Dashboard > Settings > Donorbox
+
+== Screenshots ==
+
+1. Settings page
+2. Sample form
+3. Responsive view
+
+== Changelog ==
+
+= 1.0 =
+* Initial Commit.
+
+== Upgrade Notice ==
+
+N/A.
